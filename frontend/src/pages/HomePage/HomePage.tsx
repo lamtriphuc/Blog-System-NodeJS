54370337f4647ffddbf421877cf2a51ff24a7b88
lamtriphucUTH <2251120436@ut.edu.vn>
2025-07-01 21:37:20 +0700

    layout

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <div className="">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        aliquam excepturi alias ea sapiente at quasi labore pariatur aut
+        exercitationem.
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
